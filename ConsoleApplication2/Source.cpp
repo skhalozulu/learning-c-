1e1b95416103a7c4c2c88f45ffd6e2aceef2cd3b
skhalozulu <214529134@stu.ukzn.ac.za>
2016-03-01 22:15:40 +0200

    simple calculator

---
+++ ConsoleApplication2/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std;
+
+int main()
+{
+
+	int a;
+	int b;
+	int sum;
+
+	cout << " This is the simple calculater that adds two integers \n enter the 1st integer  \n ";
+	cin >> a;
+	cout << "Enter the second integer \n";
+	cin >> b;
+	sum = a + b;
+
+	cout << "the sum is " << sum << endl;
+
+}
